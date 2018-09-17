cd788a8710ff6e2ab500cba86e68ec33a9fa155c
fabio-gariglio <garigliofabio@hotmail.com>
2018-09-17 23:31:32 +0100

    Create HttpClient with message handlers

--- src/HttpClientX/HttpClientBuilder.cs
+++ src/HttpClientX/HttpClientBuilder.cs
@@ -1,10 +1,50 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+
 namespace HttpClientX
 {
+    public delegate HttpMessageHandler HttpMessageHandlerFactoryMethod(HttpMessageHandler innerHandler);
+
     public class HttpClientBuilder
     {
+        private readonly IHttpMessageHandlerFactory _httpMessageHandlerFactory;
+        private readonly HttpClientHandler _httpClientHandler;
+        private readonly Queue<HttpMessageHandlerFactoryMethod> _httpMessageHandlerFactoryMethods;
+
+        public HttpClientBuilder()
+        {
+            _httpMessageHandlerFactory = new HttpMessageHandlerFactory();
+            _httpClientHandler = new HttpClientHandler();
+            _httpMessageHandlerFactoryMethods = new Queue<HttpMessageHandlerFactoryMethod>();
+        }
+
+        public HttpClientBuilder Use<TMessageHandler>(params object[] arguments)
+            where TMessageHandler : HttpMessageHandler
+        {
+            _httpMessageHandlerFactoryMethods.Enqueue(innerHandler => CreateFactoryMethod<TMessageHandler>(innerHandler, arguments));
+            
+            return this;
+        }
+
         public IHttpClient Build()
         {
-            return new HttpClientWrapper();
+            HttpMessageHandler innerHandler = _httpClientHandler;
+
+            foreach(var factoryMethod in _httpMessageHandlerFactoryMethods)
+            {
+                innerHandler = factoryMethod(innerHandler);
+            }
+
+            return new HttpClientWrapper(innerHandler);
+        }
+        private HttpMessageHandler CreateFactoryMethod<TMessageHandler>(HttpMessageHandler innerHandler, params object[] arguments)
+            where TMessageHandler : HttpMessageHandler
+        {
+            var constructorArguments = new[] { innerHandler }.Concat(arguments).ToArray();
+            var handler = _httpMessageHandlerFactory.Create(typeof(TMessageHandler), constructorArguments);
+
+            return handler;
         }
     }
 }
--- test/HttpClientX.Tests/HttpClientBuilderTests.cs
+++ test/HttpClientX.Tests/HttpClientBuilderTests.cs
@@ -14,5 +14,20 @@ namespace HttpClientX.Tests
             var response = await httpClient.GetAsync("http://www.google.com");
             Assert.That(response.IsSuccessStatusCode, Is.True);
         }
+
+        [Test]
+        public async Task It_should_be_possible_to_create_a_HttpClient_specifying_multiple_message_handlers()
+        {
+            var httpClient = new HttpClientBuilder()
+                .Use<SetResponseHeaderMessageHandler>("first-header", "first-value")
+                .Use<SetResponseHeaderMessageHandler>("second-header", "second-value")
+                .Build();
+
+            var response = await httpClient.GetAsync("http://www.google.com");
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+            Assert.That(response.Headers.Contains("first-header"), Is.True);
+            Assert.That(response.Headers.Contains("second-header"), Is.True);
+
+        }
     }
 }
