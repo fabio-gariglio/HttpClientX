6148c39b65fcd28673b0abf4f1c96d2af26d9c32
fabio-gariglio <garigliofabio@hotmail.com>
2018-09-17 23:42:38 +0100

    Add transient message handler tests

--- test/HttpClientX.Tests/HttpClientBuilderTests.cs
+++ test/HttpClientX.Tests/HttpClientBuilderTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
@@ -27,7 +28,24 @@ namespace HttpClientX.Tests
             Assert.That(response.IsSuccessStatusCode, Is.True);
             Assert.That(response.Headers.Contains("first-header"), Is.True);
             Assert.That(response.Headers.Contains("second-header"), Is.True);
+        }
 
+        [Test]
+        public async Task It_should_be_possible_to_get_a_new_message_handler_instance_per_request()
+        {
+            var httpClient = new HttpClientBuilder()
+                .Use<TransientMessageHandler>()
+                .Build();
+
+            var firstResponse = await httpClient.GetAsync("http://www.google.com");
+            var firstResponseHeaderValue = firstResponse.Headers.GetValues("transient-header").FirstOrDefault();
+
+            var secondResponse = await httpClient.GetAsync("http://www.google.com");
+            var secondResponseHeaderValue = secondResponse.Headers.GetValues("transient-header").FirstOrDefault();
+
+            Assert.That(firstResponseHeaderValue, Is.Not.Null);
+            Assert.That(secondResponseHeaderValue, Is.Not.Null);
+            Assert.That(firstResponseHeaderValue, Is.Not.EqualTo(secondResponseHeaderValue));
         }
     }
 }
