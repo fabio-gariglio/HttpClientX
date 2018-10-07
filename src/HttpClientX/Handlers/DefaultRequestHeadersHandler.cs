5248e44f99e0b5aec765e87668ee0860f836f056
fabio-gariglio <garigliofabio@hotmail.com>
2018-10-07 18:00:26 +0100

    Add DefaultRequestHeadersHandler

---
+++ src/HttpClientX/Handlers/DefaultRequestHeadersHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientX.Handlers
+{
+    public class DefaultRequestHeadersHandler : DelegatingHandler
+    {
+        private readonly string _headerName;
+        private readonly string _headerValue;
+
+        public DefaultRequestHeadersHandler(HttpMessageHandler innerHandler, string headerName, string headerValue) : base(innerHandler)
+        {
+            _headerName = headerName;
+            _headerValue = headerValue;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.TryAddWithoutValidation(_headerName, _headerValue);
+            
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
